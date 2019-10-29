d797d9e711d9e6bf5b4a57c9407d8205977b699b
Luis Geniole <luis.geniole@gmail.com>
2019-10-29 19:13:28 -0300

    finish componentDidMount

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repo)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repo: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repo);
+
+    // api/github.com/repos/facebook/react/
+    // api/github.com/repos/facebook/react/issues
+
+    const [repo, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repo: repo.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repo, issues, loading } = this.state;
+
+    return <h1>Repository: </h1>;
+  }
 }
